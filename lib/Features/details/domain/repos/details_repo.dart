1906a0013c1d3de25cdd2bef45c01bebce60d692
Adham-Amin <adhm0amin@gmail.com>
2025-07-15 05:21:34 +0300

    Done Details Repo

--- lib/Features/details/domain/repos/details_repo.dart
+++ lib/Features/details/domain/repos/details_repo.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:movies/Core/error/failure.dart';
 import 'package:movies/Features/details/domain/entities/details_movie_entity.dart';
+import 'package:movies/Features/home/domain/entities/movies_entity.dart';
 
 abstract class DetailsRepo {
   Future<Either<Failure, DetailsMovieEntity>> getMovieDetails({required int movieId});
-  Future<Either<Failure, DetailsMovieEntity>> getMovieSimiler({required int movieId});
+  Future<Either<Failure, List<MoviesEntity>>> getMovieSimiler({required int movieId});
 }
